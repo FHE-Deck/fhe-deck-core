de556dc1cf6308a5b9acad6cf24ef6ba4c497d13
therealkeymaker <therealkeymaker@gmail.com>
2024-10-20 15:17:42 +0200

    Includes in fhe_deck.h

--- include/fhe_deck.h
+++ include/fhe_deck.h
@@ -2,17 +2,7 @@
 #define FHE_DECK_H
 
  
-#include "interface/ciphertext.h"  
-#include "interface/fhe_context.h" 
-#include "interface/plaintext_encoding.h" 
-#include "interface/rotation_poly.h" 
-
-#include "ciphertexts/lwe.h" 
-#include "ciphertexts/ntru.h" 
-#include "ciphertexts/polynomial_ciphertext.h" 
-#include "ciphertexts/rlwe.h" 
-#include "ciphertexts/vector_ciphertext.h" 
-
+#include "fhe_context.h" 
 
 
 #endif 
