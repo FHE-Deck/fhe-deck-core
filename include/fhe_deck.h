fb821d0fad50cfe9490dea3e4091688ae8bc440d
therealkeymaker <therealkeymaker@gmail.com>
2024-10-20 15:09:08 +0200

    Added include files.

--- include/fhe_deck.h
+++ include/fhe_deck.h
@@ -1,7 +1,18 @@
 #ifndef FHE_DECK_H
 #define FHE_DECK_H
 
+ 
+#include "interface/ciphertext.h"  
+#include "interface/fhe_context.h" 
+#include "interface/plaintext_encoding.h" 
+#include "interface/rotation_poly.h" 
 
-#include "interface/fhe_context.h"
+#include "ciphertexts/lwe.h" 
+#include "ciphertexts/ntru.h" 
+#include "ciphertexts/polynomial_ciphertext.h" 
+#include "ciphertexts/rlwe.h" 
+#include "ciphertexts/vector_ciphertext.h" 
+
+
 
 #endif 
