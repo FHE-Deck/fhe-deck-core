801c26e1b0b6023088488aad2c19d9657086ed7a
therealkeymaker <therealkeymaker@gmail.com>
2024-12-10 15:10:32 +0100

    Conditional includes for intel_hexl, fftw, fftwl.

--- include/math/math.h
+++ include/math/math.h
@@ -1,9 +1,7 @@
 #ifndef MATH_H
 #define MATH_H
  
-#include "math/fftw_engine.h"
-#include "math/fftw_long_engine.h"
-#include "math/intel_hexl_engine.h"
+
 #include "math/ntl_inversion_engine.h"
 #include "math/polynomial_inversion_engine_builder.h"
 #include "math/euclidean_inversion_engine.h"
@@ -11,5 +9,16 @@
 #include "math/polynomial.h"
 #include "math/vector.h" 
 #include "math/naive_multiplication_engine.h" 
+ 
+ 
+#if defined(USE_FFTW)
+#include "math/fftw_engine.h"
+#endif 
+#if defined(USE_FFTWL)
+#include "math/fftw_long_engine.h"
+#endif 
+#if defined(USE_IntelHexl)
+#include "math/intel_hexl_engine.h"
+#endif
 
 #endif 
--- src/math/polynomial_multiplication_engine_builder.cpp
+++ src/math/polynomial_multiplication_engine_builder.cpp
@@ -2,9 +2,18 @@
 #include "math/polynomial_multiplication_engine_builder.h"
 
 #include "math/naive_multiplication_engine.h"
+
+#if defined(USE_IntelHexl)
 #include "math/intel_hexl_engine.h"
+#endif
+
+#if defined(USE_FFTW)
 #include "math/fftw_engine.h"
+#endif
+
+#if defined(USE_FFTWL)
 #include "math/fftw_long_engine.h"
+#endif 
 
 using namespace fhe_deck; 
 
